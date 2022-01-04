0aaac91df2e23902cbdf2071c6930fae45f3884b
Yash Garg <yash_fw10_103@masai.school>
2022-01-04 20:01:31 +0530

    learning...

--- react/reactrevision/src/components/Todo.jsx
+++ react/reactrevision/src/components/Todo.jsx
@@ -1,28 +1,71 @@
-// import { useState } from "react"
+import { useState } from "react";
 
-export const Todo=()=>{
-// const [title,setTitle]=useState('')
-    return(
+export const Todo = () => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState();
+  const [date, setDate] = useState();
+  const changeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const changeAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const changeDate = (e) => {
+    setDate(e.target.value);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    // making the data in object format
+    const expensedata={
+        title:title,
+        amount:amount,
+        date:new Date(date)//using new Date inbuilt to change date in date format
+    }
+    console.log(expensedata);
+    //setting inputs blank after done
+    setTitle('')
+    setAmount('')
+    setDate('')
+  };
+  return (
+    <div>
+      <h1></h1>
+      <form onSubmit={submitForm}>
         <div>
-            <h1></h1>
-            <form >
-                <div>
+          <div>
+            <label>title</label>
 
-                <div>
-                    <label >title</label>
-
-                <input type="text" placeholder="expense-name"  />
-                </div>
-                <div>
-                <label >amount</label>
-                <input type="number" placeholder="expense-amount"  />
-                </div>
-                <div>
-                <label >date</label>
-                <input type="date" placeholder="expense-date"  />
-                </div>
-                </div>
-            </form>
+            <input
+              type="text"
+              placeholder="expense-name"
+              onChange={changeTitle}
+              value={title}
+            />
+          </div>
+          <div>
+            <label>amount</label>
+            <input
+              type="number"
+              placeholder="expense-amount"
+              onChange={changeAmount}
+              value={amount}
+            />
+          </div>
+          <div>
+            <label>date</label>
+            <input
+              type="date"
+              placeholder="expense-date"
+              onChange={changeDate}
+              value={date}
+            />
+          </div>
         </div>
-    )
-}
+        <div>
+          <button type="submit">add expense</button>
+        </div>
+      </form>
+    </div>
+  );
+};
