15eeeb77f7cab0a3edcfb642def0e921aa5b2e96
Yash Garg <yash_fw10_103@masai.school>
2022-01-06 11:59:51 +0530

    solved few stack dsa

--- dsa/listAndQue1.js
+++ dsa/listAndQue1.js
@@ -1,48 +1,46 @@
 function runProgram(input) {
-    // console.log(process.env.USERNAME);
-    var ni=input.split("\n")
-    // console.log(ni);
-    var a=[]
-    var c=[]
-    for(var k=1;k<ni.length;k++){
-      var data=ni[k].trim().split(" ")
-      // console.log(data);
-      if(data[0]=='E'){
-        a.push(data[1])
+  var ni = input.split("\n");
+
+  var a = [];
+  var c = [];
+  for (var k = 1; k < ni.length; k++) {
+    var data = ni[k].trim().split(" ");
+
+    if (data[0] == "E") {
+      a.push(data[1]);
+    } else if (data[0] == "D") {
+      if (a.length != 0) {
+        var b = a.shift();
+        c.push(b);
+      } else {
+        console.log("Empty");
       }
-      else if(data[0]=='D'){
-        var b=a.shift()
-        c.push(b)
-      }
-      console.log(c);
-      
     }
-    console.log(a);
-    
-    
   }
-  if (process.env.USERNAME === "My") {
-    runProgram(`4
+  console.log(c.join("\n"));
+}
+if (process.env.USERNAME === "My") {
+  runProgram(`4
     E 2
     E 3
     D
+    D
     D`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
-  
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
