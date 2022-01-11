7bde5e9e4e897c8dd92bcf920e0c8939014ddedc
Yash Garg <yash_fw10_103@masai.school>
2022-01-11 12:26:12 +0530

    stocksBuySell

---
+++ dsa/stocksBuy&Sell.js
@@ -0,0 +1,37 @@
+function runProgram(input) {
+    // console.log(process.env.USERNAME);
+    // var ni=input.split("\n")
+    // console.log(ni);
+    var data=input.trim().split(" ").map(Number)
+    console.log(data);
+    var maxpro=0
+    var min=maxpro
+    for(var i=0;i<data.length;i++){
+        if(data[i]>min){
+            maxpro=data[i]-min
+        }
+    }
+    console.log(maxpro);
+    
+  }
+  if (process.env.USERNAME === "My") {
+    runProgram(`7 1 5 3 6 4`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
