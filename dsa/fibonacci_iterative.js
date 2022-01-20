d261b1616b51174b7f3809a8378b9b270c372b5c
Yash Garg <yash_fw10_103@masai.school>
2022-01-20 12:34:17 +0530

    revising JS

---
+++ dsa/fibonacci_iterative.js
@@ -0,0 +1,36 @@
+function runProgram(input) {
+    // var ni=input.split("\n")
+    // console.log(ni);
+    var n=Number(input)
+    // console.log(n);
+    console.log(fibonacci(n));
+    }
+    function fibonacci(n){
+        var seq=[0,1]
+        for(var i=2;i<=n;i++){
+            seq.push(seq[i-1]+seq[i-2])
+        }
+        return seq[seq.length-1]
+        
+    }
+   if (process.env.USERNAME === "My") {
+     runProgram(`4`);
+   } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   
