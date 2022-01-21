b9b09d251b7a532639423f1f02bcd105b381a909
Yash Garg <yash_fw10_103@masai.school>
2022-01-21 11:44:50 +0530

    revising JS

--- dsa/maxProduct.js
+++ dsa/maxProduct.js
@@ -1,9 +1,61 @@
-// 3
-// 6 
-// 1 0 7 2 4 0
-// 5
-// 1 2 3 4 5
-// 2
-// 0 0
-var data=[1,0,7,2,4,0]
-var max=data[0]*data[1]
+function runProgram(input) {
+    var ni=input.split("\n")
+    // console.log(ni);
+    for(var t=2;t<ni.length;t=t+2){
+        var data=ni[t].trim().split(" ").map(Number)
+        // console.log(data);
+        product(data)
+    }
+    function product(data){
+        var max=data[0]*data[1]
+var prod=1;
+var ans=[]
+for(var i=0;i<data.length;i++){
+    for(var j=0;j<data.length;j++){
+        if(i!=j){
+
+            var prod=data[i]*data[j]
+            ans.push(prod)
+        }
+    }
+}
+// console.log(ans);
+for(var k=0;k<ans.length;k++){
+    if(ans[k]>max)
+    max=ans[k]
+}
+console.log(max);
+
+    }
+    
+    
+     
+   }
+   if (process.env.USERNAME === "My") {
+     runProgram(`3
+     6 
+     1 0 7 2 4 0
+     5
+     1 2 3 4 5
+     2
+     0 0`);
+   } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   
+// var data=[1,2,3,4,5]
--- dsa/productOfArray.js
+++ dsa/productOfArray.js
@@ -1,50 +1,47 @@
 function runProgram(input) {
-    // console.log(process.env.USERNAME);
-    var ni=input.split("\n")
-    // console.log(ni);
-    for(var k=2;k<ni.length;k=k+2){
-        var arr=ni[k].trim().split(" ").map(Number)
-        // console.log(arr);
-        var product=[]
-        product.length=arr.length
-        // console.log(product.length);
-        var temp=1
-        for(var i=0;i<arr.length;i++){
-            product[i]=temp
-            temp=temp*arr[i]
-        }
-        temp=1
-        for(var i=arr.length-1;i>=0;i--){
-            product[i]=product[i]*temp
-            temp=temp*arr[i]
-        }
-        console.log(product);
-        
+  
+  var ni = input.split("\n");
+  
+  for (var k = 2; k < ni.length; k = k + 2) {
+    var arr = ni[k].trim().split(" ").map(Number);
+   
+    var product = [];
+    product.length = arr.length;
+    
+    var temp = 1;
+    for (var i = 0; i < arr.length; i++) {
+      product[i] = temp;
+      temp = temp * arr[i];
     }
+    temp = 1;
+    for (var i = arr.length - 1; i >= 0; i--) {
+      product[i] = product[i] * temp;
+      temp = temp * arr[i];
+    }
+    console.log(product);
   }
-  if (process.env.USERNAME === "My") {
-    runProgram(`2
+}
+if (process.env.USERNAME === "My") {
+  runProgram(`2
     5
     1 2 3 4 5
     3
     3 2 7`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0);
-    });
-  }
-
-  
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
