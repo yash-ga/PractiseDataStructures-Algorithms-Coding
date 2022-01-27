455bf2fdf78a1677d0da4a861234c9070bcaabed
Yash Garg <yash_fw10_103@masai.school>
2022-01-27 19:18:05 +0530

    done

---
+++ dsa/mergeSort.js
@@ -0,0 +1,79 @@
+function runProgram(input) {
+    var ni=input.split("\n")
+    console.log(ni);
+    let a=ni[1].trim().split(" ").map(Number)
+    console.log(a);
+    let l= 0;let r=a.length-1
+
+console.log(mergeSort(a,l,r));
+// console.log((merge(a,l,r,(l+r)/2)));
+
+    
+}
+function mergeSort(a,l,r){
+    if(l<r){
+        let mid=Math.floor((r+l)/2)
+        mergeSort(a,l,mid)
+        mergeSort(a,mid+1,r)
+        merge(a,l,mid,r)
+    }
+    return a
+}
+function merge(a,l,mid,r){
+    let i=l;let j=mid+1;let k=l;let b=[]
+    while(i<=mid&&j<=r){
+        if(a[i]<a[j]){
+            b[k]=a[i]
+            i++;
+        }
+        else{
+            b[k]=a[j]
+            j++
+        }
+        k++;
+    }
+
+if(i>mid){
+    while(j<=r){
+        b[k]=a[j]
+        k++;
+        j++;
+    }
+}
+else{
+    while(i<=mid){
+        b[k]=a[i]
+        k++;
+        i++;
+    }
+}
+// return b
+for(let k=l;k<=r;k++){
+    a[k]=b[k]
+}
+// return a
+}
+    
+     
+   if (process.env.USERNAME === "My") {
+     runProgram(`1
+     9 4 7 6 3 1 5`);
+   } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   
