e41bfcdaa1124d797a37c2d8dec7777ad18eeed1
Yash Garg <yash_fw10_103@masai.school>
2022-02-15 17:12:38 +0530

    solution did work

--- dsa/sumToK.js
+++ dsa/sumToK.js
@@ -1,9 +1,15 @@
 let data=[1,2,3,4,5]
 let k=6;
-let low=0;let high=data.length;
+let low=0;let high=data.length-1;
 while(low<high){
     if(data[low]+data[high]==k){
-        console.log(i,j);
+        console.log(low,high);
+    }
+    else if(data[low]+data[high]>k){
+        high--;
+    }
+    else{
+        low++;
     }
 }
 // for(let i=0;i<data.length;i++){
