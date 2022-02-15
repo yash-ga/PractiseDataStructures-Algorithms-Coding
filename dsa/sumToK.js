3950192b467888cc328e0bfff17c53864d6083b2
Yash Garg <yash_fw10_103@masai.school>
2022-02-15 17:08:50 +0530

    optimising brute force solution

--- dsa/sumToK.js
+++ dsa/sumToK.js
@@ -1,9 +1,15 @@
 let data=[1,2,3,4,5]
 let k=6;
-for(let i=0;i<data.length;i++){
-    for(let j=0;j<data.length;j++){
-        if(data[i]+data[j]==k&&i!==j){
-            console.log(i,j);
-        }
+let low=0;let high=data.length;
+while(low<high){
+    if(data[low]+data[high]==k){
+        console.log(i,j);
     }
 }
+// for(let i=0;i<data.length;i++){
+//     for(let j=0;j<data.length;j++){
+//         if(data[i]+data[j]==k&&i!==j){
+//             console.log(i,j);
+//         }
+//     }
+// }
