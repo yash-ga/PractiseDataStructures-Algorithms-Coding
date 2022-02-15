c6b39734a6c376132edd9f01b7c21451e5b45a55
Yash Garg <yash_fw10_103@masai.school>
2022-02-15 17:06:56 +0530

    revising brute force

--- dsa/sumToK.js
+++ dsa/sumToK.js
@@ -1,6 +1,9 @@
 let data=[1,2,3,4,5]
+let k=6;
 for(let i=0;i<data.length;i++){
     for(let j=0;j<data.length;j++){
-        
+        if(data[i]+data[j]==k&&i!==j){
+            console.log(i,j);
+        }
     }
 }
