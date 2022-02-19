b1715711578139e72f02562382fa6dba2d9f016c
Yash Garg <yash_fw10_103@masai.school>
2022-02-19 13:59:05 +0530

    selection sort completed

--- dsa/SortingAlgorithms/SelectionSort.js
+++ dsa/SortingAlgorithms/SelectionSort.js
@@ -1,1 +1,13 @@
 let data=[9,8,7,6,5];
+for(let i=0;i<data.length;i++){
+    let min=i;
+    for(let j=i+1;j<data.length;j++){
+        if(data[i]>data[j]){
+            min=j;
+            let temp=data[i]
+            data[i]=data[min]
+            data[min]=temp;
+        }
+    }
+}
+console.log(data);
