b8307897c6e756af92b33b541814af4370c21f0c
Yash Garg <yash_fw10_103@masai.school>
2022-02-20 18:32:45 +0530

    finding frequency of an element using binary search

--- dsa/BinarySearchAlgorithms/searchAnElement.js
+++ dsa/BinarySearchAlgorithms/searchAnElement.js
@@ -1,18 +1,20 @@
+// console.log('yash');
 let data=[5,4,3,2,1]
 let k=3;
 let low=0;let high=data.length-1;let ans=-1;
 function search(low,high,ans){
 
     while(low<=high){
-        let mid=Math.floor(low+(high-low)/2)
+        let mid=low+(Math.floor((high-low)/2))
+        // console.log(mid);
         if(data[mid]==k){
         ans=mid
     }
     else if(data[mid]>k){
-        low=mid+1;
+        high=mid-1
     }
     else{
-        high=mid-1
+        low=mid+1;
     }
 }
 return ans
