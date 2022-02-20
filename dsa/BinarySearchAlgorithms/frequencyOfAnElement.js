3914c6721c8033d8b1ab70735129fb904256396b
Yash Garg <yash_fw10_103@masai.school>
2022-02-20 18:53:33 +0530

    finfing frequency of a number using binary search  algorithm done

--- dsa/BinarySearchAlgorithms/frequencyOfAnElement.js
+++ dsa/BinarySearchAlgorithms/frequencyOfAnElement.js
@@ -1,7 +1,48 @@
 let data=[1,2,3,3,3,4,5]
-let low=0;
-let high=data.length-1;
-function lowerBound(low,high){
-    
+let k=3;
+function lowerBound(data,k){
+    let low=0;
+    let high=data.length-1;
+    let ans=-1;
+    while(low<=high){
+        let mid=low+(Math.floor(high-low)/2)
+        if(data[mid]==k){
+            ans=mid
+            low=mid+1
+        }
+        else if(data[mid]>k){
+            high=mid-1
+        }
+        else{
+            low=mid+1
+        }
+    }
+    return ans
+
 
 }
+function upperBound(data,k){
+    let low=0;
+    let high=data.length-1;
+    let ans=-1;
+    while(low<=high){
+        let mid=low+(Math.floor(high-low)/2)
+        if(data[mid]==k){
+            ans=mid
+            high=mid-1
+        
+        }
+        else if(data[mid]>k){
+            high=mid-1
+        }
+        else{
+            low=mid+1
+        }
+    }
+    return ans
+
+
+}
+let a=lowerBound(data,k)
+let b=upperBound(data,k)
+console.log(a-b+1);
