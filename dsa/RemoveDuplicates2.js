24148c2e7620ef50cdb110430da32cb14975e774
Yash Garg <yash_fw10_103@masai.school>
2022-02-27 17:47:16 +0530

    removeduplicate using sort

--- dsa/RemoveDuplicates2.js
+++ dsa/RemoveDuplicates2.js
@@ -1,16 +1,13 @@
-let data='xxyzz'
-let count=1;
+let data='aabbcd'
+
+let data1=(data.split("").sort());
 let res=''
-for(let i=1;i<data.length;i++){
-    // let temp=data[i-1]
-    if(data[i]==data[i+1]){
-        count++
-        // res=res+temp+count;
-        // count=1;
+for(let i=0;i<data1.length;i++){
+    if(data1[i]==data1[i+1]){
+        continue;
     }
     else{
-        res=res+data[i]+count;
-        count=1
+        res=res+data1[i]
     }
 }
 console.log(res);
